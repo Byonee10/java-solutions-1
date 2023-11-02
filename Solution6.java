8dc7427dc752d5206c8025abbad6d4863676ca78
Byonee10 <65749103+Byonee10@users.noreply.github.com>
2023-11-02 17:21:13 +0300

    Comment ekleme

--- Solution6.java
+++ Solution6.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 
 public class Solution6 {
     public static void main(String[] args) {
+        //Girdi almak için scanner objesi yaratıyoruz.
         Scanner scanner = new Scanner(System.in);
+        // Girdi istediğimizi belirtiyoruz.
         System.out.print("Bir film türü veya 1-3 arasında numara girin: ");
+        //Girdimizi okuyor.
         String turadi = scanner.nextLine();
-
+        //Turadi'ya atadığımız değeri kontrol ediyoruz ve filmleri printliyoruz.
+        // 1 ila 3 arası değer de alabilir alternatif için.
         if (turadi.equalsIgnoreCase("aksiyon")|| turadi.equals("1")){
             System.out.println("Tür adı: Aksiyon");
             System.out.println("Matrix");
@@ -24,11 +28,13 @@ public class Solution6 {
             System.out.println("Forrest Gump");
             System.out.println("The Green Mile");
         }
+        // Eğer girdimiz aksiyon/dram/komedi veya 1-3 arası numara değilse bunu printliyoruz.
+        
         else {
             System.out.print("Geçerli tür ismi veya numara giriniz");
         
         }
-
+        // Scanner'ı kapatıyoruz. Gerekli değil ama istediği için yaptım.
         scanner.close();
     }
 }
